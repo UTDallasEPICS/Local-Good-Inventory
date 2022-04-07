dec1eaffa29098c4f31b897ca8237048c8aac440
Al Wasee Mahmood <79488226+Al-Wasee-Mahmood@users.noreply.github.com>
2022-04-07 15:18:48 -0500

    home page changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { CheckinComponent } from './checkin/checkin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CheckinComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  currentYear = this.getYear();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getYear(): number {
+    var d = new Date(); 
+    const year = d.getFullYear(); 
+    return year;
+  }
 }
