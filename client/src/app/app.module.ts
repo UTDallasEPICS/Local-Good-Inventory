9ef27e7c4393a14f04955a66f1ccbafac21a397b
Al Wasee Mahmood <79488226+Al-Wasee-Mahmood@users.noreply.github.com>
2022-04-21 15:02:19 -0500

    New changes checkin page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxMaskModule } from 'ngx-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CheckinComponent } from './checkin/checkin.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { CheckinComponent } from './checkin/checkin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/checkin/checkin.component.ts
+++ client/src/app/checkin/checkin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-checkin',
@@ -7,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckinComponent implements OnInit {
 
+  mobNumberPattern = "^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$";  
+  isValidFormSubmitted = false;  
+  user = new User();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  OnlyNumbersAllowed(event: { which: any; keyCode: any; }):boolean
+   {
+    const charCode = (event.which)?event.which: event.keyCode;
+
+    if (charCode > 31 && (charCode < 48 || charCode >57))
+    {
+      console.log('charCode restricted is' + charCode);
+      return false;
+    }
+
+    return true;
+  }
+
+    
+
+  onFormSubmit(form: NgForm) {  
+    this.isValidFormSubmitted = false;  
+    if (form.invalid) {  
+       return;  
+    }  
+    this.isValidFormSubmitted = true;  
+    form.resetForm();  
+ }
   
+  
+    
 }
+
+export class User {  
+  mobileNumber ?: string;  
+ }  
