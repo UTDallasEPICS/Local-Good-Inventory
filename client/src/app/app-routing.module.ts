5782da45736791246cf9fc3ad07c91a3e157f6f5
Al Wasee Mahmood <79488226+Al-Wasee-Mahmood@users.noreply.github.com>
2022-04-28 15:45:33 -0500

    new-registration changes 2

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { CheckinComponent } from './checkin/checkin.component';
 import { HomeComponent } from './home/home.component';
 import { AppointmentComponent } from './Appointment/appointment.component'; 
 import { Appointment2Component } from './Appointment2/appointment2.component'; 
+import { NewRegistrationComponent } from './new-registration/new-registration.component';
 
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'checkin', component:CheckinComponent},
+  {path:'new-registration', component:NewRegistrationComponent},
   {path:'Appointment', component:AppointmentComponent},
   {path:'Appointment2', component:Appointment2Component}
 ];
