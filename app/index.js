ef87c78a7a4d810713835af881439fc60f04fba2
mpross512 <mpross512@gmail.com>
2022-05-05 14:05:54 -0500

    Merged with new pages

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 const mongoClient = new MongoClient("mongodb+srv://user1:UTDallas1@cluster0.yjnoy.mongodb.net/")
@@ -13,17 +14,19 @@ mongoClient.connect();
 
 const familiesCollection = mongoClient.db('TestDatabase').collection('CollectionOne');
 
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Header', 
+    'Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
-app.use('/family', (req, res) => {
+app.get('/family', (req, res) => {
+  console.log("Get request");
   family = {phoneNumber: "", name: "", members: [] }
   
   if(req.query.phoneNumber) {
@@ -38,6 +41,18 @@ app.use('/family', (req, res) => {
     res.status(404);
   }
   
+});
+
+app.post('/family', (req, res) => {
+  if(req.query.phoneNumber) {
+    const query = { phoneNumber: req.query.phoneNumber };
+    const newValue = { $set: { checkedIn: req.body.checkedIn, nextAppointment: req.body.nextAppointment } };
+    familiesCollection.updateOne(query, newValue);
+    res.status(201);
+  } else {
+    res.status(404);
+  }
+
 });
 
 app.listen(port, () => {
--- client/src/app/pages/new-registration/new-registration.component.ts
+++ client/src/app/pages/new-registration/new-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Family } from 'src/app/models/family.model';
 
 @Component({
   selector: 'app-new-registration',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-registration.component.css']
 })
 export class NewRegistrationComponent implements OnInit {
+
+  private family: Family = {phoneNumber: "", name: "", members: [], checkedIn: [], nextAppointment: ""};
 
   constructor() { }
 
