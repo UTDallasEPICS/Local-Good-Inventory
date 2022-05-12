58aeec15125e7dfd31a706761dcb79765e8698bb
mpross512 <mpross512@gmail.com>
2022-05-12 13:51:36 -0500

    Fixed website crashing when updating family

--- app/index.js
+++ app/index.js
@@ -46,7 +46,13 @@ app.get('/family', (req, res) => {
 app.post('/family', (req, res) => {
   if(req.query.phoneNumber) {
     const query = { phoneNumber: req.query.phoneNumber };
-    const newValue = { $set: req.body };
+    const newValue = { $set: { 
+      name: req.body.name,
+      members: req.body.members,
+      allergies: req.body.allergies,
+      phoneNumber: req.body.phoneNumber,
+      checkedIn: req.body.checkedIn,
+      nextAppointment: req.body.nextAppointment } };
     familiesCollection.updateOne(query, newValue, {upsert: true});
     res.status(201);
   } else {
