d3f290e8d895df0ec06df4ce8ac600bb1e8dd352
mpross512 <mpross512@gmail.com>
2022-09-14 15:55:58 -0500

    Updated UI Elements and Routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { RemoveFamilyMemberComponent } from './pages/Removing-Family-Member/remo
 import { NewRegistrationComponent } from './pages/new-registration/new-registration.component';
 import { CommonModule } from '@angular/common';
 import { CheckinCompleteComponent } from './pages/checkin-complete/checkin-complete.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { CheckinCompleteComponent } from './pages/checkin-complete/checkin-compl
     FamilyOrMemberComponent,
     RemoveFamilyMemberComponent,
     NewRegistrationComponent,
-    CheckinCompleteComponent
+    CheckinCompleteComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
