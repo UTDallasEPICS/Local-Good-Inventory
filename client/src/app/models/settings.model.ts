18049bb55a810c793f68fa09b7fb80291f7cbb02
mpross512 <mpross512@gmail.com>
2022-09-28 16:35:48 -0500

    Added settings model and settings service

---
+++ client/src/app/models/settings.model.ts
@@ -0,0 +1,7 @@
+export interface Settings {
+    dates: string[]; //Days of the week that are valid for appointments
+    startTime: string; //Time appointments should start on each day
+    endTime: string; //Time appointments should end on each day
+    interval: number; //Time in minutes each appointment should last (15 minutes)
+    quantity: number; //Number of peoople who can sign up for each appointment slot
+}
---
+++ client/src/app/services/settings.service.ts
@@ -0,0 +1,46 @@
+import { Settings } from "../models/settings.model"
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class FamilyService {
+    private settings: Settings = 
+        {
+            dates: ['F','S'],
+            startTime: "9:30am",
+            endTime: "11:30am",
+            interval: 15,
+            quantity: 4
+        };
+    private settingsUpdated = new Subject<Settings>();
+
+    constructor(private http: HttpClient) { }
+
+    getFamily() {
+        return {...this.settings};
+    }
+
+    getFamilyUpdateListener() {
+        return this.settingsUpdated.asObservable();
+    }
+
+    updateFamily() {
+        const promiseToken = new Promise((resolve, reject) => {
+            this.http.get<{settings: Settings}>(`http://localhost:3000/settings`)
+            .subscribe((settings) => {
+                    this.settings = settings.settings;
+                    this.settingsUpdated.next({...this.settings});
+                    resolve(settings);
+            });    
+        });
+
+        return promiseToken;
+        
+    }
+
+    postFamily(settings: Settings) {
+        this.http.post(`http://localhost:3000/settings`, this.settings)
+            .subscribe();
+    }
+}
