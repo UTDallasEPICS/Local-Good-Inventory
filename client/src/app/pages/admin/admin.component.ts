b3e4a9f20fb781309c58b54a414f9fd2faed21ff
mpross512 <mpross512@gmail.com>
2022-10-19 00:51:43 -0500

    Fixed admin page bug

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Appointment } from 'src/app/models/appointment.model';
 
 @Component({
   selector: 'app-admin',
@@ -9,26 +8,12 @@ import { Appointment } from 'src/app/models/appointment.model';
 })
 export class AdminComponent implements OnInit {
 
-  private appointment: Appointment = {
-    date: "09-23-2022",
-    startTime: "9:30am",
-    endTime: "11:30am",
-    quantity: 4,
-    interval: 15,
-    timeslots: []
-  }
-
   public pages = [{page: "Stats", active: true}, 
                   {page: "Settings", active: false}];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
-
-  addAppointment() {
-    this.http.post(`http://localhost:3000/appointment?date=${this.appointment.date}`, this.appointment)
-      .subscribe();
   }
 
   setActive(id: number) {
