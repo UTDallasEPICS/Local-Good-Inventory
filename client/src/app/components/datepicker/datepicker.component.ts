1309e9fcb65c7f89448da43f0a3075d2e28d6efb
mpross512 <mpross512@gmail.com>
2022-11-02 12:03:57 -0500

    Finished Calendar UI

--- client/src/app/components/datepicker/datepicker.component.ts
+++ client/src/app/components/datepicker/datepicker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'datepicker',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatepickerComponent implements OnInit {
 
-  constructor() { }
+  @Input() selectedDates: string[] = [];
+  @Output() selectedDatesChange = new EventEmitter();
 
+  @Input() selectedDate: string = "";
+  @Output() selectedDateChange = new EventEmitter();
+
+  @Input() multiSelect: boolean = false;
+
+  today: Date = new Date();
+  pipe = new DatePipe('en-US');
+  
+  selectedMonth = this.today.getMonth();
+  selectedYear = this.today.getFullYear();
+  
+  dates = Array(this.daysInMonth(this.selectedMonth, this.today.getFullYear())).fill(0).map((x,i)=>i+1);
+  monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  constructor() { }
+  
   ngOnInit(): void {
+    for(var i = 0; i < this.firstDayOfMonth(); i++) {
+      this.dates.unshift(0);
+    }
+  }
+
+  changeMonth(month: number) {
+    if(month == 13) {
+      this.selectedYear++;
+      month = 1;
+    } else if(month == 0) {
+      this.selectedYear--;
+      month = 12;
+    }
+    this.selectedMonth = month;
+    this.dates = Array(this.daysInMonth(this.selectedMonth, this.selectedYear)).fill(0).map((x,i)=>i+1);
+    for(var i = 0; i < this.firstDayOfMonth(); i++) {
+      this.dates.unshift(0);
+    }
+  }
+
+  daysInMonth (month: number, year: number): number {
+    return new Date(year, month, 0).getDate();
+  }
+
+  firstDayOfMonth(): number {
+    return new Date(this.selectedYear, this.selectedMonth - 1, 1).getDay();
+  }
+
+  dateSelected(date: number) {
+    let dateString = this.pipe.transform(new Date(this.selectedYear, this.selectedMonth - 1, date).toDateString(), 'MM-dd-YYYY');
+    if(dateString) {
+      if(this.multiSelect)
+        return this.selectedDates.includes(dateString);
+      return this.selectedDate == dateString;
+    }
+    return false;
+  }
+
+  selectDate(date: number) {
+    let dateString = this.pipe.transform(new Date(this.selectedYear, this.selectedMonth - 1, date).toDateString(), 'MM-dd-YYYY');
+    if(dateString != null) {
+      if(this.multiSelect) {
+        if(this.dateSelected(date)) {
+          this.selectedDates.splice(this.selectedDates.indexOf(dateString), 1);
+        } else {
+          this.selectedDates.push(dateString);
+        }
+      } else if(this.dateSelected(date)) {
+        this.selectedDate = "";
+      } else {
+        this.selectedDate = dateString;
+      }
+    }
+    this.selectedDateChange.emit(this.selectedDate);
+    this.selectedDatesChange.emit(this.selectedDates);
   }
 
   
