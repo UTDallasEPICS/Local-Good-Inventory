f4d3c48c2a8899acc4f2bd1442f0f585a38de58f
mpross512 <mpross512@gmail.com>
2022-11-15 17:56:23 -0600

    Added edit family functionality

--- client/src/app/pages/family-edit/family-edit.component.ts
+++ client/src/app/pages/family-edit/family-edit.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Family } from 'src/app/models/family.model';
 import { FamilyService } from 'src/app/services/family.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import * as Constants from 'src/app/models/constants.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-family-edit',
@@ -10,20 +16,46 @@ import { FamilyService } from 'src/app/services/family.service';
 })
 export class FamilyEditComponent implements OnInit {
 
-    family: Family = {name: "", phoneNumber: "", members: [], allergies: [], checkedIn: [], nextAppointment: "" };
+    family: Family = {name: "", phoneNumber: "", members: [], allergies: [], checkedIn: [], nextAppointment: "", color:"" };
     private familySubscription: Subscription = new Subscription;
+
+    public restrictionsVisble = false;
+    public editMode = false;
+
+    dietaryRestrictions = Constants.dietaryRestrictions;
   
-    constructor(public familyService: FamilyService) { }
+    constructor(public http: HttpClient, public familyService: FamilyService, private router: Router) { }
   
     ngOnInit() {
-      //this.family = this.familyService.getFamily();
+      this.family = this.familyService.getFamily();
       this.familySubscription = this.familyService.getFamilyUpdateListener()
         .subscribe((family: Family) => {
-          //this.family = family;
+          this.family = family;
         });
     }
     ngOnDestroy() {
       this.familySubscription.unsubscribe();
+    }
+
+    updateFamily(): void {
+      if(this.editMode) {
+        if(this.family.phoneNumber.length == 10) {
+          this.http.post(`http://localhost:3000/family?phoneNumber=${this.family.phoneNumber}`, this.family)
+          .subscribe();
+          console.log(this.family);
+        }
+        this.editMode = false;
+        this.router.navigate(['/appointment']);
+      } else {
+        this.editMode = true;
+      }
+    }
+
+    updateRestriction(restriction: string) {
+      if(!this.family.allergies.includes(restriction))
+        this.family.allergies.push(restriction);
+      else
+        this.family.allergies.splice(this.family.allergies.indexOf(restriction), 1);
     }
   
   }
