71ac232cfe957d743564805fd9e88b81b87b259a
Aarian <aarianahsan@gmail.com>
2022-11-16 16:35:02 -0600

    Added console log to buttondisplay

--- client/src/app/pages/Appointment/appointment.component.ts
+++ client/src/app/pages/Appointment/appointment.component.ts
@@ -1,37 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { Family } from 'src/app/models/family.model';
 import { FamilyService } from 'src/app/services/family.service';
+import { Settings } from 'src/app/models/settings.model';
+import { SettingsService } from 'src/app/services/settings.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'appointment',
   templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.css']
+  styleUrls: ['./appointment.component.css'],
 })
 export class AppointmentComponent implements OnInit {
-
   family: Family = {} as Family;
-  nextAppointment: string = "";
+  settings: Settings = {} as Settings;
+  private settingsSubscription: Subscription = new Subscription();
 
-  constructor(public familyService: FamilyService) { }
+  nextAppointment: string = '';
+
+  constructor(
+    public familyService: FamilyService,
+    private settingsService: SettingsService
+  ) {}
 
   isDisplay = false;
   clicked = false;
 
-  toggle()
-  {
+  toggle() {
     this.isDisplay = true;
     this.clicked = true;
   }
 
   ngOnInit(): void {
+    this.settings = this.settingsService.getSettings();
+    this.settingsService.updateSettings();
+    this.settingsSubscription = this.settingsService
+      .getSettingsUpdateListener()
+      .subscribe((settings: Settings) => {
+        this.settings = settings;
+      });
   }
+  //make appointment time show up on front end
+  //date string to date obj
 
+  //day of week from day obj
+  //output start and end time for that specific date && week of the settings obj
+  buttonDisplay() {
+    window.alert(this.settings);
+  }
   updateAppointment() {
     this.family = this.familyService.getFamily();
     this.family.nextAppointment = this.nextAppointment;
     this.familyService.postFamily(this.family);
     console.log();
   }
-
-  
 }
