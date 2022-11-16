9d636929c805f4d4854abf5c409b460ba155b439
Aarian <aarianahsan@gmail.com>
2022-11-16 16:47:15 -0600

    michael fixed something and now it works

--- client/src/app/pages/Appointment/appointment.component.ts
+++ client/src/app/pages/Appointment/appointment.component.ts
@@ -12,15 +12,10 @@ import { Subscription } from 'rxjs';
 })
 export class AppointmentComponent implements OnInit {
   family: Family = {} as Family;
-<<<<<<< HEAD
   settings: Settings = {} as Settings;
   private settingsSubscription: Subscription = new Subscription();
-=======
-  nextAppointment: string = "";
-  nextAppointmentTime: string = "T10:00";
->>>>>>> f4d3c48c2a8899acc4f2bd1442f0f585a38de58f
-
   nextAppointment: string = '';
+  nextAppointmentTime: string = 'T10:00';
 
   constructor(
     public familyService: FamilyService,
@@ -54,8 +49,9 @@ export class AppointmentComponent implements OnInit {
   }
   updateAppointment() {
     this.family = this.familyService.getFamily();
-    this.family.nextAppointment = this.nextAppointment + this.nextAppointmentTime;
+    this.family.nextAppointment =
+      this.nextAppointment + this.nextAppointmentTime;
     this.familyService.postFamily(this.family);
-    console.log("Next appointment: " + this.family.nextAppointment);
+    console.log('Next appointment: ' + this.family.nextAppointment);
   }
 }
