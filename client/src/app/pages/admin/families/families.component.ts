a9ee750c30b2386f0b0ee9c21ede20412a167e19
mpross512 <mpross512@gmail.com>
2022-12-27 01:29:35 -0500

    Updated more API calls

--- client/src/app/pages/admin/families/families.component.ts
+++ client/src/app/pages/admin/families/families.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Family } from 'src/app/models/family.model';
 import * as Constants from 'src/app/models/constants.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-families',
@@ -37,7 +38,7 @@ export class FamiliesComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("OnInit");
-    this.http.get<{families: Family[]}>(`http://localhost:3000/family`)
+    this.http.get<{families: Family[]}>(`http://${environment.API_URL}/family`)
       .subscribe((families) => {
           this.families = families.families;
           this.filteredFamilies = families.families.filter(t=>t);
@@ -78,7 +79,7 @@ export class FamiliesComponent implements OnInit {
   updateFamily(): void {
     if(this.editMode) {
       if(this.selectedFamily.phoneNumber.length == 10) {
-        this.http.post(`http://localhost:3000/family?phoneNumber=${this.selectedFamily.phoneNumber}`, this.selectedFamily)
+        this.http.post(`http://${environment.API_URL}/family?phoneNumber=${this.selectedFamily.phoneNumber}`, this.selectedFamily)
         .subscribe();
         console.log(this.selectedFamily);
       }
@@ -101,7 +102,7 @@ export class FamiliesComponent implements OnInit {
 
   deleteFamily() {
     if(this.selectedFamily.phoneNumber.length == 10) {
-      this.http.delete(`http://localhost:3000/family?phoneNumber=${this.selectedFamily.phoneNumber}`)
+      this.http.delete(`http://${environment.API_URL}/family?phoneNumber=${this.selectedFamily.phoneNumber}`)
         .subscribe();
     }
     this.modalVisible = false;
